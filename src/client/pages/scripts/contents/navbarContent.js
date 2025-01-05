02009a4db76b4eb5517ba58fa7a605fb271d631d
Erangamadhushan <erangamadhushann956@gmail.com>
2025-01-06 03:31:37 +0530

    Add Bootstrap integration and navbar structure to multiple pages

--- src/client/pages/scripts/contents/navbarContent.js
+++ src/client/pages/scripts/contents/navbarContent.js
@@ -1,1 +1,18 @@
-export const navbarContent = []
+export const navbarContent = [
+    {
+        title:"Home",
+        link:"./index.html"
+    },
+    {
+        title:"About",
+        link:"./aboutUs.html"
+    },
+    {
+        title:"Contact",
+        link:"./contactUs.html"
+    },
+    {
+        title:"Services",
+        link:'./services.html'
+    }
+]
