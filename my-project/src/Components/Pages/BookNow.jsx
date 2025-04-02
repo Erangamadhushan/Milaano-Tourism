4d4aa04353012f62cbaffb94f0b6e1615fba2347
Erangamadhushan <erangamadhushann956@gmail.com>
2025-04-03 03:59:09 +0530

    feat: Refactor BookNow and RoomCategory components; update booking logic and enhance UI layout

--- my-project/src/Components/Pages/BookNow.jsx
+++ my-project/src/Components/Pages/BookNow.jsx
@@ -1,19 +1,72 @@
 import react from 'react';
 import { useState } from 'react';
 
-const BookNow = ({roomDetails}) => {
+const BookNow = () => {
+    const roomDetails = JSON.parse(localStorage.getItem("roomDetails"));
     return (
         <>
-            <div className="w-[95%] max-w-[1000px] mx-auto p-5 my-10 ">
-                <div className=''>
-                    <p className='text-center text-[1.25em] md:text-[1.5em] text-blue-500 font-bold'>---- Book Now ----</p>
-                    <h2 className="text-[2em] text-center"> Book Your <span className='text-blue-500'>ROOM</span></h2>
+            <div className='w-[95%] grid grid-cols-1 md:grid-cols-2 max-w-[1200px] mx-auto my-5 p-5'>
+                <div className='grid'>
+                    <div>
+                        <img src={roomDetails.room_image} alt="" className='w-full h-[300px] object-cover' />
+                    </div>
+                    <div className='grid grid-cols-1  gap-5 mt-5'>
+                        <div className='bg-white p-2 rounded-lg'>
+                            <p className='text-[1.5em] font-bold text-blue-500'>{roomDetails.room_type} <br/><span className='text-[1.6em] font-bold text-blue-500'>{`  Rs. `}{roomDetails.room_price} {`.00`}</span></p>
+                            <p className='text-gray-500'>{roomDetails.room_description}</p>
+                            <p className='text-blue-500 py-4'>Room Features</p>
+                            <ul>
+                                {
+                                    roomDetails.room_features.map((feature, index) => (
+                                        <li key={index} className='text-gray-500'>{` => `}{feature}</li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex flex-col items-center justify-center w-full h-full p-5 my-10 bg-white rounded-lg shadow-lg">
-                    <h2 className="text-[1.25em] py-2 font-semibold text-blue-500">{roomDetails.room_type}</h2>
-                    <p className=" py-2">Price: Rs.{roomDetails.room_price}.00</p>
-                    <p className=" py-2">Capacity: {roomDetails.room_capacity}</p>
-                    <p className=" py-2">Description: {roomDetails.room_description}</p>
+                <div>
+                    
+                <div className="grid grid-cols-1 p-5 py-3">
+                    <div>
+                        <h3 className="text-3xl text-center text-blue-500 font-bold">Check Out</h3>
+                    </div>
+                    <div className="grid grid-cols-1 py-4">
+                        <div className="p-2">
+                            <p>Name :</p>
+                        </div>
+                        <div className="grid grid-cols-2 gap-2">
+                            <div>
+                                <input type="text" placeholder="First Name" className="border border-gray-200 p-2 rounded-lg w-[85%]" />
+                            </div>
+                            <div>
+                                <input type="text" placeholder="Last Name" className="border border-gray-200 p-2 rounded-lg w-[85%]" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 py-4">
+                        <div className="p-2">
+                            <p>E-mail :</p>
+                        </div>
+                        <div className="grid grid-cols-1 gap-2">
+                            <div>
+                                <input type="text" placeholder="example@gmail.com" className="border border-gray-200 p-2 rounded-lg w-[85%]" />
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1">
+                        <div className="p-2">
+                            <p>Address :</p>
+                        </div>
+                        <div className="grid grid-cols-1 gap-2">
+                            <div>
+                                <input type="text" placeholder="Araliya Mawatha, Maddawaththa, Matara" className="border border-gray-200 p-2 rounded-lg w-[85%]" />
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </>
--- my-project/src/Components/Pages/RoomCategory.jsx
+++ my-project/src/Components/Pages/RoomCategory.jsx
@@ -3,16 +3,17 @@ import { useState } from 'react';
 import BookNow from './BookNow';
 
 const RoomCategory = ({roomDetails}) => {
-
-    const handBookRoom = ({roomDetails}) => {
-        <BookNow roomDetails={roomDetails} />
-        //alert("Room Booked Successfully!");
+    const handleBookNow = () => {
+        // Handle the booking logic here
+        console.log("Booking room:", roomDetails);
+        localStorage.setItem("roomDetails", JSON.stringify(roomDetails));
     }
+    
     return (
         <>
             <div className="w-[95%] min-h-[400px] max-w-[380px] scroll-up">
                 <div className="p-5 gap-2 w-full">
-                    {/* Sample image here .. */}
+                    
                     <img src={roomDetails.room_image} className="w-[100%] h-[200px] object-cover " alt={roomDetails.room_type} />
                 </div>
                 <div className="p-5 gap-2 w-[95%] max-w-[450px]">
@@ -21,7 +22,10 @@ const RoomCategory = ({roomDetails}) => {
                     <p className=" py-2">Capacity: {roomDetails.room_capacity}</p>
                     <p className=" py-2">Description: {roomDetails.room_description}</p>
                     
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" onClick={handBookRoom}>Book Now</button>
+                    <div className="flex gap-x-5">
+                        <a href="/booknow"  onClick={handleBookNow} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" >Book Now</a>
+                        <a href="/viewdetails"  onClick={handleBookNow} className="border border-blue-500 hover:bg-blue-700 text-blue-500 hover:text-white font-bold py-2 px-4 rounded-lg" >View Details</a>
+                    </div>
                 </div>
                 
             </div>
