62536c02d9378a02594a060b935d15b66bb2ad84
Erangamadhushan <erangamadhushann956@gmail.com>
2025-06-01 16:22:08 +0530

    feat: Refactor Header and Footer components for improved site navigation and information display

---
+++ my-project/client/src/Components/layout/Footer.jsx
@@ -0,0 +1,56 @@
+import { NavLink } from "react-router-dom"
+export default function Footer() {
+    return (
+        <div className="grid">
+            <div className="grid grid-cols-2 bg-zinc-700 md:grid-cols-4 custom:grid-cols-2">
+                <div className="grid p-5 py-10">
+                    <a href="https://www.srilanka.travel/" className="text-white hover:text-zinc-900" target="_blank">Sri Lanka Travel</a>
+                    <a href="https://nationalholidayresorts.lk/" className="text-white hover:text-zinc-900" target="_blank">National Holiday Resorts</a>
+                    <a href="https://www.slithm.edu.lk/" target="_blank" className="text-white hover:text-zinc-900">Sri Lanka Institude of Tourism & Hotel Management</a>
+                    <a href="https://www.worldtravelawards.com/" target="_blank" className="text-white hover:text-zinc-900">Tourism Awards</a>
+                </div>
+                <div className="grid p-5 py-10 items-center">
+                    <a target="_blank" className="text-white hover:text-zinc-900" href="https://www.google.lk/maps/@7.8417541,80.7728782,7.75z">Site Map</a>
+                    <a href="/aboutus" className="text-white hover:text-zinc-900">Complaints</a>
+                </div>
+                <div className="bg-bg-mainColor p-3 py-10">
+                    <div className="grid gap-1">
+                        <a href="/contact" className="text-white hover:text-zinc-900 font-bold text-xl">Contact Us</a>
+                        <h4  className="text-white font-bold text-xl">Address</h4>
+                        <p className="text-white">
+                            Sri Lanka Tourism Development
+                            Authority
+                            No 80.Galle road.
+                            Colombo 03
+                        </p>
+                        <h4 className="text-white font-bold text-xl">Telephone</h4>
+                        <p className="text-white">
+                            +94 112345678/
+                            0123456789
+                        </p>
+                    </div>
+                </div>
+
+                <div className="bg-bg-mainColor p-3 py-10">
+                    <div className="text-white">
+                        <h4 className="font-bold text-xl">Fax</h4>
+                        <p>
+                            +94 112 444165/2426984
+                        </p>
+                        <h4 className="font-bold text-xl">Email</h4>
+                        <p>
+                            info@beautysrilanka.travel
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="bg-zinc-800 p-3">
+                <h1 className="text-white text-center">Powered by EM956 Community</h1>
+            </div>
+            
+            
+        </div>
+    )
+}
+
--- my-project/client/src/Layout.jsx
+++ my-project/client/src/Layout.jsx
@@ -1,11 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from './Components/Header';
-import { useEffect } from "react";
 import Footer from './Components/Footer';
 
-
-
-// import withReactContent from '@sweetalert2/react-content';
 
 export default function Layout() {
     return (
