aee26249f5b6b42c803e30281a105f2bc0faf25b
Erangamadhushan <erangamadhushann956@gmail.com>
2025-06-03 15:04:31 +0530

    feat: Refactor AboutUs and BookNow pages to utilize new components for improved structure and readability

---
+++ my-project/client/src/Components/ui/About/Vision&Mission.jsx
@@ -0,0 +1,28 @@
+export function Vision() {
+    return (
+        <div className="min-h-[50vh] max-w-[600px] mx-auto p-3 scroll-reveal" >
+            <h1 className=" text-[2em] md:text-[3em] font-bold text-zinc-500 py-5 text-center">Our Vision</h1>
+            <p class="text-lg text-center">To inspire and empower people to explore the world, create unforgettable memories, and foster a deeper understanding of diverse cultures through seamless and accessible travel experiences.</p>
+        </div>
+    )
+}
+
+export function Mission() {
+    return (
+        <div className="min-h-[50vh] max-w-[600px] mx-auto p-3 scroll-items">
+            <h1 className="text-[2em] md:text-[3em] font-bold py-5 text-zinc-500 text-center">Our Mission</h1>
+            <p class="text-lg text-center">Our mission is to be the ultimate travel companion, providing comprehensive, reliable, and personalized resources that cater to every traveler's needs. We aim to simplify the journey from planning to booking, offering expert advice, curated experiences, and innovative tools that turn dream destinations into reality. Through our commitment to sustainability and cultural respect, we strive to make travel not just an adventure, but a force for positive change.</p>
+        </div>
+    )
+}
+
+export function AboutIntro() {
+    return (
+        <div className="p-5 w-[95%] max-w-[1200px] mx-auto flex justify-center items-center" >
+            <div className="min-h-[50vh]  p-3 scroll-up">
+                <h1 className="text-[3em] font-bold text-zinc-500 text-center">About Us</h1>
+                <p class="text-lg p-5 text-center">About We want to create a one platform that helps you plan your trip to any destination. Whether you are a local or a foreign traveler, you will find everything you need to know about your chosen place. You will get a general overview of Sri Lanka and its current situation. You will learn about the weather, the attractions, and the dos and don'ts of each location. You will also discover the best ways to get around and the nearest facilities like police stations, ATMs, banks, pharmacies, and more. Our platform is your one-stop guide to enjoy your travel experience.</p>
+            </div>
+        </div>
+    )
+}
--- my-project/client/src/Pages/BookNow.jsx
+++ my-project/client/src/Pages/BookNow.jsx
@@ -3,6 +3,10 @@ import { useState } from 'react';
 import { useRef } from 'react';
 import Swal from 'sweetalert2';
 
+//-----------------------------------------------------------------------------------------------
+// Importing the required  components
+import  BookingRoomForm  from '../Components/forms/BookingRoomForm';
+
 const BookNow = () => {
     const formRef = useRef(null);
     const roomDetails = JSON.parse(localStorage.getItem("roomDetails"));
@@ -92,70 +96,16 @@ const BookNow = () => {
                         </div>
                     </div>
                 </div>
+
                 <div>
-                    
-                <div className="grid grid-cols-1 p-5 py-3">
-                    <div>
-                        <h3 className="text-3xl text-center text-zinc-600 font-bold">Check Out</h3>
+                    <div className="grid grid-cols-1 p-5 py-3">
+                        <div>
+                            <h3 className="text-3xl text-center text-zinc-600 font-bold">Check Out</h3>
+                        </div>
+                        
+                        <BookingRoomForm formRef={formRef} formSubmission={formSubmission} />
+                        
                     </div>
-                    <form ref={formRef} onSubmit={formSubmission} className="grid grid-cols-1">
-                        <div className="grid grid-cols-1 py-4">
-                            
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-                                <div>
-                                <div className="p-2">
-                                    <p>First Name :</p>
-                                </div>
-                                    <input type="text" placeholder="First Name" name='firstname' className="border border-zinc-600 p-2 rounded-lg w-[95%] mx-auto"  required/>
-                                </div>
-                                <div>
-                                <div className="p-2">
-                                    <p>Last Name :</p>
-                                </div>
-                                    <input type="text" placeholder="Last Name" name="lastname" className="border border-zinc-600 p-2 rounded-lg w-[95%] mx-auto" required/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-1 py-4">
-                            <div className="p-2">
-                                <p>E-mail :</p>
-                            </div>
-                            <div className="grid grid-cols-1 gap-2">
-                                <div>
-                                    <input type="email" placeholder="example@gmail.com" name='email' className="border border-zinc-600 p-2 mx-auto rounded-lg w-[95%]" required/>
-                                </div>
-                                
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-1">
-                            <div className="p-2">
-                                <p>Address :</p>
-                            </div>
-                            <div className="grid grid-cols-1 gap-2">
-                                <div>
-                                    <input type="text" name='address' placeholder="Araliya Mawatha, Maddawaththa, Matara" className="border border-zinc-600 p-2 rounded-lg w-[95%] mx-auto" required/>
-                                </div>
-                                
-                            </div>
-                            <div className="grid grid-cols-1 gap-2 py-4">
-                                <div>
-                                    <label htmlFor='phononumber'>Phone Number :</label>
-                                    <input type="tel" name='phonenumber' placeholder="011-1212321" className="border border-zinc-600 mx-auto p-2 rounded-lg w-[95%]" required/>
-                                </div>
-                                
-                            </div>
-                            <div className="grid grid-cols-1 gap-2 py-4">
-                                <div>
-                                    <label htmlFor='special_request'>Special Request :</label><br/>
-                                    <textarea rows="8" name='request' placeholder="Any special request you have" className="border border-zinc-600 w-[95%] mx-auto resize-none p-2 rounded-lg"></textarea>
-                                </div>
-                                <div className='w-full  text-white'>
-                                    <button type="submit" className="bg-zinc-600 w-[95%] mx-auto text-center text-white p-2 py-3 text-xl rounded-lg">Complete Booking</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
                 </div>
             </div>
         </>
